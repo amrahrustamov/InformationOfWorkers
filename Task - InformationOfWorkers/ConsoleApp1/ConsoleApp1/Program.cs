42dd22a620feebacfd4d8eca7906e5aad326166e
Asus <Asus@DESKTOP-IJEPJQM>
2023-04-02 01:18:39 +0400

    Updated Name_Surname_FatherName_Age

--- Task - InformationOfWorkers/ConsoleApp1/ConsoleApp1/Program.cs
+++ Task - InformationOfWorkers/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsoleApp1
+﻿using System.Xml.Linq;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
@@ -10,65 +12,94 @@
 
             bool resultUpperCaseLetters = false;
             bool resultLowerLetters = false;
+            bool resultNumbers = false;
             string name;
             string surname;
             string fatherName;
-            byte age;
+            string age;
             string personalNo;
             byte phoneNumber;            
             string jobPosition;
             double salary;          
 
-            Console.Write("The number of employees to be added to the system :");
+            Console.Write("Count of employees : ");
             int count = int.Parse(Console.ReadLine());
 
             for(int i = 0; i < count; i++)
             {
                 Console.Write("Add worker's name: ");
                 name = Console.ReadLine();
-                Name(upperCaseLetters, lowerLetters, resultUpperCaseLetters, resultLowerLetters, name);
-                Surname();
-                FatherName();
-                Age();
+                Name(upperCaseLetters, lowerLetters, numbers, resultUpperCaseLetters, resultLowerLetters, resultNumbers, name);
+
+                Console.Write("Add worker's surname: ");
+                surname = Console.ReadLine();
+                Surname(upperCaseLetters, lowerLetters, numbers, resultUpperCaseLetters, resultLowerLetters, resultNumbers, surname);
+
+                Console.Write("Add worker's father's name: ");
+                fatherName = Console.ReadLine();
+                FatherName(upperCaseLetters, lowerLetters, numbers, resultUpperCaseLetters, resultLowerLetters, resultNumbers, fatherName);
+
+                Console.Write("Add worker's age: ");
+                age = Console.ReadLine();
+                Age(numbers, age);
+
                 PhoneNumber();
                 JobPosition();
                 Salary();
             }
         }
-        public static void Name(char[] upperCaseLetters, char[] lowerLetters, bool resultUpperCaseLetters, bool resultLowerLetters, string name)
+        public static void Name(char[] upperCaseLetters, char[] lowerLetters, char[] numbers, bool resultUpperCaseLetters, bool resultLowerLetters, bool resultNumbers, string name)
         {
             int sum = 1;
-
-            if (name.Length < 20 && name.Length > 2)
-            {
+           if (name.Length < 20 && name.Length > 2)
+           {
                 for (int j = 0; j < name.Length; j++)
-                {                 
-                    if(j == 0)
+                {
+                    for (int k = 0; k < name.Length; k++)
                     {
-                      for (int a = 0; a < upperCaseLetters.Length; a++)
-                      {
-                          if (name[j] == upperCaseLetters[a])
-                          {
+                        for (int g = 0; g < numbers.Length; g++)
+                        {
+                            if (name[k] == numbers[g])
+                            {
+                                resultNumbers = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (resultNumbers == true)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Name not accepted!");
+                        Console.WriteLine("The name must contain only letters");
+                        Console.ReadLine();
+                        break;
+                    }
+                    if (j == 0)
+                    {
+                        for (int a = 0; a < upperCaseLetters.Length; a++)
+                        {
+                            if (name[j] == upperCaseLetters[a])
+                            {
                                 resultUpperCaseLetters = true;
                                 j++;
                                 break;
-                          }
-                      }
+                            }
+                        }
                     }
-                    if(j < name.Length)
+                    if (j < name.Length)
                     {
-                        for(int a = 0; a < lowerLetters.Length; a++)
+                        for (int a = 0; a < lowerLetters.Length; a++)
                         {
                             if (name[j] == lowerLetters[a])
-                            {                                
+                            {
                                 sum = sum + 1;
-                                if(sum == name.Length)
-                                    resultLowerLetters = true;                              
+                                if (sum == name.Length)
+                                    resultLowerLetters = true;
                             }
                         }
                     }
                 }
-                if(resultLowerLetters == true && resultUpperCaseLetters == true)
+                if (resultLowerLetters == true && resultUpperCaseLetters == true)
                 {
                     Console.WriteLine("");
                     Console.WriteLine("Name added successfully");
@@ -79,7 +110,153 @@
                     Console.WriteLine("");
                     Console.WriteLine("The name is not entered correctly!");
                     Console.WriteLine("The first letter should be uppercase and the following letters should be lowercase");
+                    Console.ReadLine();
+                }
+           }
+           else
+           {
+               Console.WriteLine("");
+               Console.WriteLine("The name is not entered correctly!");
+               Console.WriteLine("Letters must be greater than 2 and less than 20");
+               Console.ReadLine();
+           }
+        }
+        public static void Surname(char[] upperCaseLetters, char[] lowerLetters, char[] numbers, bool resultUpperCaseLetters, bool resultLowerLetters, bool resultNumbers, string surname)
+        {
+            int sum = 1;
+            if (surname.Length < 35 && surname.Length > 2)
+            {
+                for (int j = 0; j < surname.Length; j++)
+                {
+                    for (int k = 0; k < surname.Length; k++)
+                    {
+                        for (int g = 0; g < numbers.Length; g++)
+                        {
+                            if (surname[k] == numbers[g])
+                            {
+                                resultNumbers = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (resultNumbers == true)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Surname not accepted!");
+                        Console.WriteLine("The Surname must contain only letters");
+                        Console.ReadLine();
+                        break;
+                    }
+                    if (j == 0)
+                    {
+                        for (int a = 0; a < upperCaseLetters.Length; a++)
+                        {
+                            if (surname[j] == upperCaseLetters[a])
+                            {
+                                resultUpperCaseLetters = true;
+                                j++;
+                                break;
+                            }
+                        }
+                    }
+                    if (j < surname.Length)
+                    {
+                        for (int a = 0; a < lowerLetters.Length; a++)
+                        {
+                            if (surname[j] == lowerLetters[a])
+                            {
+                                sum = sum + 1;
+                                if (sum == surname.Length)
+                                    resultLowerLetters = true;
+                            }
+                        }
+                    }
+                }
+                if (resultLowerLetters == true && resultUpperCaseLetters == true)
+                {
                     Console.WriteLine("");
+                    Console.WriteLine("Surname added successfully");
+                    Console.WriteLine("");
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("The surname is not entered correctly!");
+                    Console.WriteLine("The first letter should be uppercase and the following letters should be lowercase");
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("The surname is not entered correctly!");
+                Console.WriteLine("Letters must be greater than 2 and less than 35");
+                Console.ReadLine();
+            }
+        }
+        public static void FatherName(char[] upperCaseLetters, char[] lowerLetters, char[] numbers, bool resultUpperCaseLetters, bool resultLowerLetters, bool resultNumbers, string fatherName)
+        {
+            int sum = 1;
+            if (fatherName.Length < 20 && fatherName.Length > 2)
+            {
+                for (int j = 0; j < fatherName.Length; j++)
+                {
+                    for (int k = 0; k < fatherName.Length; k++)
+                    {
+                        for (int g = 0; g < numbers.Length; g++)
+                        {
+                            if (fatherName[k] == numbers[g])
+                            {
+                                resultNumbers = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (resultNumbers == true)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Name not accepted!");
+                        Console.WriteLine("The name must contain only letters");
+                        Console.ReadLine();
+                        break;
+                    }
+                    if (j == 0)
+                    {
+                        for (int a = 0; a < upperCaseLetters.Length; a++)
+                        {
+                            if (fatherName[j] == upperCaseLetters[a])
+                            {
+                                resultUpperCaseLetters = true;
+                                j++;
+                                break;
+                            }
+                        }
+                    }
+                    if (j < fatherName.Length)
+                    {
+                        for (int a = 0; a < lowerLetters.Length; a++)
+                        {
+                            if (fatherName[j] == lowerLetters[a])
+                            {
+                                sum = sum + 1;
+                                if (sum == fatherName.Length)
+                                    resultLowerLetters = true;
+                            }
+                        }
+                    }
+                }
+                if (resultLowerLetters == true && resultUpperCaseLetters == true)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Name added successfully");
+                    Console.WriteLine("");
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("The name is not entered correctly!");
+                    Console.WriteLine("The first letter should be uppercase and the following letters should be lowercase");
+                    Console.ReadLine();
                 }
             }
             else
@@ -87,21 +264,35 @@
                 Console.WriteLine("");
                 Console.WriteLine("The name is not entered correctly!");
                 Console.WriteLine("Letters must be greater than 2 and less than 20");
+                Console.ReadLine();
+            }
+        }
+        public static void Age(char[] numbers, string age)
+        {            
+            try
+            {
+                 int convertAge = Convert.ToInt32(age);
+                 if(convertAge >= 18 && convertAge <= 65)
+                 {    
+                         Console.WriteLine("");
+                         Console.WriteLine("Age added successfully");
+                         Console.WriteLine("");                     
+                 }
+                 else
+                 {
+                     Console.WriteLine("");
+                     Console.WriteLine("Age not accepted!");
+                     Console.WriteLine("Age should be between 18-65 years");
+                     Console.WriteLine("");
+                 }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Age not accepted!");
+                Console.WriteLine("Age must contain only numbers");
                 Console.WriteLine("");
             }
-            
-        }
-        public static void Surname()
-        {
-            Console.Write("Add worker's surname: ");
-        }
-        public static void FatherName()
-        {
-            Console.Write("Add worker's father's name: ");
-        }
-        public static void Age()
-        {
-            Console.Write("Add worker's age: ");
         }
         public static void PhoneNumber()
         {
